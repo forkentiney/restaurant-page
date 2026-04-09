0d81e86e59adf6a76b53bc9b7da9750e810cab17
Forkentiney <jseverud@proton.me>
2026-04-09 11:12:26 -0500

    Rewrote menu to be less verbose
    Menu items are now populated from an array.

--- src/menu.js
+++ src/menu.js
@@ -1,65 +1,44 @@
 import "./menu.css";
 
+function createItem(item, description) {
+  const name = item;
+  const details = description;
+  return { name, details };
+};
+
+const sourdough = createItem("Sourdough", "Starter, flour, water, salt. Simple and effective.");
+const bagels = createItem("Bagels", "Made to emulate the classic NYC bagel, our dough is boiled and baked into an easily recognizable shape.");
+const granola = createItem("Granola", "Oaty and nutty, a perfect match for your daily yogurt or ice cream.");
+const cookies = createItem("Cookies", "Sweet and perfectly baked, share these cookies with your friends and family or eat them alone. We don't judge.");
+
+const menuItems = [sourdough, bagels, granola, cookies,];
+
 function createMenu() {
   console.log("Creating menu");
 
   const menuContainer = document.createElement("div");
   menuContainer.setAttribute("id", "menu");
   menuContainer.classList.add("flex-centered");
+  content.appendChild(menuContainer);
 
   const menuTitle = document.createElement("h1");
   menuTitle.textContent = "Menu";
-
-  const menuSourdough = document.createElement("div")
-  menuSourdough.classList.add("menu-item");
-  menuSourdough.classList.add("flex-centered");
-  const menuSourdoughTitle = document.createElement("h3");
-  menuSourdoughTitle.textContent = "Sourdough";
-  const menuSourdoughBody = document.createElement("p");
-  menuSourdoughBody.textContent = "Starter, flour, water, salt. Simple and effective.";
-
-  const menuBagel = document.createElement("div")
-  menuBagel.classList.add("menu-item");
-  menuBagel.classList.add("flex-centered");
-  const menuBagelTitle = document.createElement("h3");
-  menuBagelTitle.textContent = "Bagels";
-  const menuBagelBody = document.createElement("p");
-  menuBagelBody.textContent = "Made to emulate the classic NYC bagel, our dough is boiled and baked into an easily recognizable shape.";
-
-  const menuGranola = document.createElement("div")
-  menuGranola.classList.add("menu-item");
-  menuGranola.classList.add("flex-centered");
-  const menuGranolaTitle = document.createElement("h3");
-  menuGranolaTitle.textContent = "Granola";
-  const menuGranolaBody = document.createElement("p");
-  menuGranolaBody.textContent = "Oaty and nutty, a perfect match for your daily yogurt or ice cream.";
-
-  const menuCookies = document.createElement("div")
-  menuCookies.classList.add("menu-item");
-  menuCookies.classList.add("flex-centered");
-  const menuCookiesTitle = document.createElement("h3");
-  menuCookiesTitle.textContent = "Cookies";
-  const menuCookiesBody = document.createElement("p");
-  menuCookiesBody.textContent = "Sweet and perfectly baked, share these cookies with your friends and family or eat them alone. We don't judge.";
-
-  content.appendChild(menuContainer);
   menuContainer.appendChild(menuTitle);
-  menuContainer.appendChild(menuSourdough);
-  menuContainer.appendChild(menuBagel);
-  menuContainer.appendChild(menuGranola);
-  menuContainer.appendChild(menuCookies);
 
-  menuSourdough.appendChild(menuSourdoughTitle);
-  menuSourdough.appendChild(menuSourdoughBody);
+  menuItems.forEach((item) => {
+    const menu = document.createElement("div");
+    menu.classList.add("menu-item", "flex-centered");
 
-  menuBagel.appendChild(menuBagelTitle);
-  menuBagel.appendChild(menuBagelBody);
+    const title = document.createElement("h3");
+    title.textContent = item.name;
 
-  menuGranola.appendChild(menuGranolaTitle);
-  menuGranola.appendChild(menuGranolaBody);
+    const body = document.createElement("p");
+    body.textContent = item.details;
 
-  menuCookies.appendChild(menuCookiesTitle);
-  menuCookies.appendChild(menuCookiesBody);
+    menuContainer.appendChild(menu);
+    menu.appendChild(title);
+    menu.appendChild(body);
+  });
 };
 
 export { createMenu };
