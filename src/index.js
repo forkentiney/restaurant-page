db1a58cf90432e5ad8c05a57b3defd4e0e246efc
Forkentiney <jseverud@proton.me>
2026-04-09 15:55:21 -0500

    Gallery now reorganizes on page resize
    This is done with a delay to prevent too many reloads when page is being
    manually resized.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createGallery } from "./gallery.js";
 import { createContact } from "./contact.js";
 import { createMenu } from "./menu.js";
 
+window.addEventListener("resize", checkState);
+
 const content = document.querySelector("#content");
 const home = document.querySelector("#home");
 const menu = document.querySelector("#bakes");
@@ -12,30 +14,48 @@ const photos = document.querySelector("#photos");
 const about = document.querySelector("#about");
 const contact = document.querySelector("#contact");
 
+const state = { page: "home" };
+
 home.addEventListener("click", () => {
   clearContent();
   createHome();
+  state.page = "home";
 });
 
 about.addEventListener("click", () => {
   clearContent();
   createAbout();
+  state.page = "about";
 });
 
 photos.addEventListener("click", () => {
   clearContent();
   createGallery();
+  state.page = "gallery";
 });
 
 contact.addEventListener("click", () => {
   clearContent();
   createContact();
+  state.page = "contact";
 });
 
 menu.addEventListener("click", () => {
   clearContent();
   createMenu();
+  state.page = "menu";
 });
+
+function reloadGallery() {
+  clearContent();
+  createGallery();
+};
+
+function checkState() {
+  if (state.page === "gallery") {
+    setTimeout(reloadGallery, 500);
+  };
+};
 
 function clearContent() {
   console.log("Clearing body");
