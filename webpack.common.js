8720a1f5e63ce2bb327f349be3ae0927875fe168
Forkentiney <jseverud@proton.me>
2026-09-06 11:16:45 -0400

    Split webpack configs
    More streamlined dev/production flow

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,11 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
